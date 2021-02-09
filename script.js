3fb8967dc3ebad40866dd73441974bfaf2b9e000
Morten Gross <mg@vuffeli.dk>
2021-02-09 16:31:25 +0100

    I can count with a timed delay

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 
 window.addEventListener("DOMContentLoaded", start);
 
+const array = [];
+let counter = 0;
+
 function start() {
     console.log("start");
+
+    counter++;
+    console.log(counter);
+
+    setTimeout(start, 1000);
+
 }
